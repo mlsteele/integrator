67d25e154a58e979f58ac4e09043809ee27b3337
Miles Steele <msteele@edx.org>
2013-06-12 17:51:51 -0400

    hack in web response

--- demo.py
+++ demo.py
@@ -2,40 +2,56 @@ from elements import *
 from strategies import *
 from parser import parse
 
+class Logger(object):
+  def __init__(self):
+    self.buffer = ''
+
+  def log(self, string):
+    self.buffer += str(string) + '\n'
+
+  def dump(self):
+    return self.buffer
+
+logger = Logger()
+
 def attempt_intgral(expr):
-  print "I will attempt to solve %s." % expr
+  logger.log("I will attempt to solve %s." % expr)
   expr = expr.simplified()
-  print "I can simplify it to %s." % expr
+  logger.log("I can simplify it to %s." % expr)
 
   if isinstance(expr, Integral):
-    print "%s is an integral" % expr
+    logger.log("%s is an integral" % expr)
 
     for strategy in STRATEGIES:
       if strategy.applicable(expr):
-        print "I will use the %s rule" % strategy.description
+        logger.log("I will use the %s rule" % strategy.description)
         return attempt_intgral(strategy.apply(expr))
 
-    print "I think I'm stuck."
+    logger.log("I think I'm stuck.")
     return expr
   elif isinstance(expr, Sum):
-    print "%s is a sum" % expr
-    print "I will solve the two sub-problems of %s and %s" %(expr.a, expr.b)
-    print ""
-    print "subproblem:"
+    logger.log("%s is a sum" % expr)
+    logger.log("I will solve the two sub-problems of %s and %s" %(expr.a, expr.b))
+    logger.log("")
+    logger.log("subproblem:")
     sub_a = attempt_intgral(expr.a)
-    print "subproblem:"
+    logger.log("subproblem:")
     sub_b = attempt_intgral(expr.b)
     return Sum(sub_a, sub_b)
   else:
-    print "%s is not an integral, I think I'm stuck." % expr
+    logger.log("%s is not an integral, I think I'm stuck." % expr)
     return expr
 
-if __name__ == "__main__":
+def run():
   ss = ["intxdx", "intx^2dx", "int 3 x^(2 * 3) dx", "int x + 3 dx"]
   for s in ss:
     expr = parse(s)
-    print expr
-    print "\n end result: %s" % attempt_intgral(expr)
-    print 2 * "\n"
-    print "-----"
-    print 2 * "\n"
+    logger.log(expr)
+    logger.log("\n end result: %s" % attempt_intgral(expr))
+    logger.log(2 * "\n")
+    logger.log("-----")
+    logger.log(2 * "\n")
+
+if __name__ == "__main__":
+  run()
+  print logger.dump()
--- web.py
+++ web.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask import app, make_response, render_template
 app = Flask(__name__)
 
+import demo
 
 @app.route("/")
-def hello():
-  return "Hello World!"
+def index():
+  # return '<a href"http://localhost:5000/demo">Demo</a>'
+  resp = make_response('<a href="/demo">Demo</a>')
+  resp.mimetype = 'text/html'
+  return resp
 
+@app.route("/demo")
+def hello():
+  demo.run()
+  return demo.logger.dump().replace('\n', '<br>')
 
 if __name__ == "__main__":
   app.run()
