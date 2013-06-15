07b0b0691eda7cfb936205b575177f3deacb6258
Miles Steele <miles@milessteele.com>
2013-06-15 11:40:08 -0400

    refactor tree logger to preserve order

--- treelogger.py
+++ treelogger.py
@@ -1,27 +1,29 @@
 class TreeLogger(object):
+  """ Logger class that knows how to deal with tree-like logs """
   def __init__(self, title='root', parent=None):
+    self.parent = parent
     self.title = title
     self._entries = []
-    self.parent = parent
-    self._children = []
+    # entries e.g. [(level, logger, payload)]
 
-  def push(self, msg):
-    self._entries.append(msg)
+  def push(self, payload):
+    """ Add payload to the log """
+    self._push(0, self, payload)
 
-  def split(self, title='children'):
-    logger = TreeLogger(title, parent=self)
-    self._children.append(logger)
-    return logger
+  def _push(self, level, log, payload):
+    if not self.parent:
+      self._entries.append((level, log, payload))
+    else:
+      self.parent._push(level + 1, log, payload)
+
+  def split(self, title='child'):
+    log = TreeLogger(title, parent=self)
+    return log
 
   def dump(self):
-    return '\n'.join([
-      '{}{}) {}'.format('  '*level, '.'.join(reversed([log.title] + log.parent_titles())), entry)
-        for (level, log, entry) in self._dump_list()])
-
-  def format(level, log, entry):
-    return '\n'.join([
-      '{}{}) {}'.format('  '*level, '.'.join(reversed([log.title] + log.parent_titles())), entry)
-        for (level, log, entry) in self._dump_list()])
+    dot_titles = lambda log: '.'.join(reversed([log.title] + log.parent_titles()))  
+    return '\n'.join(['{}{}) {}'.format('  '*level, dot_titles(log), payload)
+      for (level, log, payload) in self._entries])
 
   def parent_titles(self):
     if self.parent:
@@ -29,69 +31,23 @@ class TreeLogger(object):
     else:
       return []
 
-  def _dump_list(self):
-    """ List of tuples (level, TreeLogger, entry) """
-    entries = [(0, self, entry) for entry in self._entries]
-    for child in self._children:
-      entries += [(level + 1, log, entry) for (level, log, entry) in child._dump_list()]
-    return entries
 
+if __name__ == '__main__':
+  t1 = TreeLogger()
+  t1.push('foo')
+  t2 = t1.split('split-a')
+  t2.push('foo_a')
+  t2.push('foo_a2')
+  t2_2 = t2.split('higher')
+  t2_2.push('ooh')
+  t2_2.push('yeah')
+  t3 = t1.split('split-b')
+  t3.push('foo_b')
+  t1.push('and back to the root')
+  t1.push('we\'re done here')
 
-class LameLogger(object):
-  def info(self, msg):
-    print msg
+  print t1.dump()
 
-
-class PrefixLogger(object):
-  def __init__(self, prefix=''):
-    self.prefix = prefix
-
-  def info(self, msg):
-    print self.prefix + msg
-
-  def split(self, prefix):
-    return PrefixLogger(prefix)
-
-
-
-
-## example
-# def tree_op(obj, log, stop=False):
-#   log.info('{} foo'.format(obj))
-
-#   if not stop:
-#     tree_op('{}.a'.format(obj), log, stop=True)
-#     tree_op('{}.b'.format(obj), log, stop=True)
-
-
-# tree_op('obj', LameLogger())
-
-
-
-# def tree_op(obj, log, stop=False):
-#   log.info('{} foo'.format(obj))
-#   if not stop:
-#     tree_op('{}.a'.format(obj), log.split('  a) '), stop=True)
-#     tree_op('{}.b'.format(obj), log.split('  b) '), stop=True)
-
-
-# tree_op('obj', PrefixLogger())
-
-
-
-t1 = TreeLogger()
-t1.push('foo')
-t2 = t1.split('split-a')
-t2.push('foo_a')
-t2.push('foo_a2')
-t2_2 = t2.split('higher')
-t2_2.push('ooh')
-t2_2.push('yeah')
-t3 = t1.split('split-b')
-t3.push('foo_b')
-
-print t1.dump()
-
-print t1.parent_titles()
-print t2.parent_titles()
-print t2_2.parent_titles()
+  print t1.parent_titles()
+  print t2.parent_titles()
+  print t2_2.parent_titles()
